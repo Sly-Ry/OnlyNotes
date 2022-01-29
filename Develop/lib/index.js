16d58fee20582be758911abcfcf66df9ba13688e
ryan <ryan.williams93@aol.com>
2022-01-29 14:55:08 -0600

    created functions in lib folder

---
+++ Develop/lib/index.js
@@ -0,0 +1,41 @@
+const fs = require('fs');
+const path = require('path');
+
+// 
+function filterByQuery(query, notesArray) {
+    let filteredResults = notesArray;
+    
+    if(query.title) {
+        filteredResults = filteredResults.filter(note => note.title === query.title);
+    }
+    if(query.text) {
+        filteredResults = filteredResults.filter(note => note.text === query.text);
+    }
+};
+// Find notes by their id
+function findById(id, notesArray) {
+    const results = notesArray.filter(note => note.id === id)[0];
+    return results;
+};
+
+// Creates a new note
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, '../db/db.json'),
+        JSON.stringify({ notes: notesArray}, null, 2)
+    );
+    return note;
+};
+
+// Guarentees a note will not be created unless certain criteria are met
+function validateNote(note) {
+    if (!note.title || typeof note.title !== 'string') {
+        return false;
+    }
+    if (!note.text || typeof note.text !== 'string') {
+        return false;
+    }
+    return true;
+};
