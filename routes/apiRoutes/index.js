e61699a3bef62b361c3eedaa2f9c1429c06faa63
ryan <ryan.williams93@aol.com>
2022-01-30 21:07:23 -0600

    some delete functionality

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const router = require('express').Router();
 const { 
     findById, 
@@ -24,27 +25,36 @@ router.get('/notes/:id', (req, res) => {
 
 // A route that listens for POST request/ 
 router.post('/notes', (req, res) => {
-    req.body.id = note.length.toString();
+    req.body.id = notes.length.toString();
 
-    // add animal to json file and note array in this function
-    if (!validateAnimal(req.body)) {
+    if (!validateNote(req.body)) {
         res.status(400).send('The note is not properly formatted.');
     }
     else {
-        const animal = createNewAnimal(req.body, note);
-        res.json(animal);
+        const note = createNewNote(req.body, notes);
+        res.json(note);
     }
 });
 
-// router.delete('/notes/:id', (req, res) => {
-//     const result = req.params.id;
+router.delete('/notes/:id', (req, res) => {
+    console.log('deleted');
+    const result = req.params.id;
 
-//     if(result) {
-//         res.json(result);
-//     }
-//     else {
-//         res.send(404);
-//     }
-// });
+    let filteredResult = notes.filter(function (note) {
+        return note.id != result;
+    });
+    
+    pickleNotes = { notes:filteredResult }
+    let parsedNotes = JSON.stringify(pickleNotes);
+    // pickleNotes = filteredResult;
+
+    console.log(parsedNotes);
+
+    fs.writeFileSync(__dirname + '/../../db/db.json', parsedNotes, (err) => {
+        if (err) throw err;
+    });
+
+    res.end();
+});
 
 module.exports = router;
